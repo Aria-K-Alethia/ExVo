2913e3a7bcec6f8afa1cf4778639bc1b195be1f8
Aria-K-Alethia <xindetai@qq.com>
2022-05-15 05:27:31 +0900

    1. add logger; 2. improve details; 3. add model checkpoint callbacks; 3. alter the model (layernorm to batchnorm)

--- lightning_module.py
+++ lightning_module.py
@@ -2,26 +2,30 @@ import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 import pytorch_lightning as pl
+from metric import CCC
 
 class BaselineLightningModule(pl.LightningModule):
     def __init__(self, cfg):
         super().__init__()
         self.cfg = cfg
+        self.lr = cfg.train.lr
         self.construct_model()
     
     def construct_model(self):
         feat_dim = self.cfg.model.feat_dim
         
         self.model = nn.Sequential(
+                        nn.BatchNorm1d(feat_dim),
                         nn.Linear(feat_dim, 1024),
-                        nn.LayerNorm(1024),
+                        nn.BatchNorm1d(1024),
                         nn.LeakyReLU(),
                         nn.Linear(1024, 256),
-                        nn.LayerNorm(256),
+                        nn.BatchNorm1d(256),
                         nn.LeakyReLU(),
                         nn.Linear(256, 64),
-                        nn.LayerNorm(64),
+                        nn.BatchNorm1d(64),
                         nn.LeakyReLU(),
                         nn.Linear(64, 10),
                         nn.Sigmoid()
@@ -37,7 +41,7 @@ class BaselineLightningModule(pl.LightningModule):
         gt_emotion = batch['emotion']
         pred_emotion = self(feats)
         loss = F.l1_loss(pred_emotion, gt_emotion, reduction='mean')
-        self.log("train_loss", loss)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True, batch_size=feats.shape[0])
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -45,9 +49,23 @@ class BaselineLightningModule(pl.LightningModule):
         gt_emotion = batch['emotion']
         pred_emotion = self(feats)
         loss = F.l1_loss(pred_emotion, gt_emotion, reduction='mean')
-        self.log("val_loss", loss)
-        return loss
+
+        # collect results and metrics
+        out = {'gt_emotion': gt_emotion.detach().cpu(), 'pred_emotion': pred_emotion.detach().cpu(), 'loss': loss.detach().cpu()}
+
+        return out
+
+    def validation_epoch_end(self, outputs):
+        val_loss = torch.stack([out['loss'] for out in outputs]).mean().item()
+        gt_emotion = torch.cat([out['gt_emotion'] for out in outputs], 0)
+        pred_emotion = torch.cat([out['pred_emotion'] for out in outputs], 0)
+        ccc = CCC(pred_emotion, gt_emotion)
+        self.log('val_loss', val_loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log('val_ccc', ccc, on_epoch=True, prog_bar=True, logger=True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), 1e-4)
-        return optimizer
+        optimizer = optim.Adam(self.parameters(), self.cfg.train.lr)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=1, min_lr=1e-6, verbose=True)
+        scheduler_config = {'scheduler': scheduler, 'interval': 'epoch', 'frequency': 1, 'monitor': 'val_ccc'}
+        return {'optimizer': optimizer, 'lr_scheduler': scheduler_config}
+    
--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 import pytorch_lightning as pl
 import hydra
 from pytorch_lightning import seed_everything
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers.csv_logs import CSVLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from torch.utils.data import DataLoader
 from dataset import DataModule
 from lightning_module import BaselineLightningModule
@@ -10,9 +14,21 @@ seed_everything(seed)
 
 @hydra.main(config_path='config', config_name='default')
 def train(cfg):
+    # loggers
+    csvlogger = CSVLogger(save_dir=cfg.train.log_dir, name='csv')
+    tblogger = TensorBoardLogger(save_dir=cfg.train.log_dir, name='tb')
+    loggers = [csvlogger, tblogger]
+
+    # callbacks
+    checkpoint_callback = ModelCheckPoint(dirpath=cfg.train.log_dir, 
+                            save_top_k=1, save_last=True,
+                            every_n_epochs=1, monitor='val_ccc', mode='max')
+    callbacks = [checkpoint_callback]
+
     datamodule = DataModule(cfg)
     lightning_module = BaselineLightningModule(cfg)
-    trainer = pl.Trainer(gpus=1,  max_epochs=10, min_epochs=5)
+    trainer = pl.Trainer(**cfg.train.trainer, logger=loggers, callbacks=callbacks)
+    #trainer.tune(lightning_module, datamodule=datamodule)
     trainer.fit(lightning_module, datamodule=datamodule)
 
 
