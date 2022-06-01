da0c430264460dd539615747108141e9665f7c14
Aria-K-Alethia <xindetai@qq.com>
2022-06-01 12:07:31 +0900

    confusion matrix analysis code

--- analyze.py
+++ analyze.py
@@ -1,11 +1,15 @@
 import torch
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from os.path import join, basename, exists
 from metric import CCC
+from sklearn.metrics import confusion_matrix
+from collections import Counter
 
-root = '/scratch/acc12076sp/baseline'
-model = 'compare'
+root = '/scratch/acc12076sp/main'
+model = 'xlsr-chain-h2l-aug-test'
 val = pd.read_csv('../data/data_info.csv')
 print(val.shape)
 val = val[val.Split != 'Test']
@@ -33,3 +37,21 @@ s1 = torch.Tensor([val_scores[id_] for id_ in ids])
 s2 = torch.Tensor([pred_scores[id_] for id_ in ids])
 print(s1.shape, s2.shape)
 print(CCC(s2, s1, True))
+
+s1 = s1.numpy().argmax(1)
+s2 = s2.numpy().argmax(1)
+cm = confusion_matrix(s1, s2)
+#cm = pd.DataFrame(data=cm, index=emotions, columns=emotions)
+labels = ['Awe', 'Excite.', 'Amuse.', 'Awkward.', 'Fear', 'Horror', 'Distress', 'Triumph', 'Sadness', 'Surprise']
+plt.figure(figsize=(12, 12))
+sns.heatmap(cm/cm.sum(1, keepdims=True), square=True, annot=True,
+            fmt='.2%', cmap='Blues', cbar=False,
+            xticklabels=labels, yticklabels=labels)
+plt.yticks(rotation=0)
+plt.ylabel('GT', fontsize=12, rotation=0)
+plt.xlabel('Prediction', fontsize=12)
+#plt.tight_layout()
+plt.savefig('conf_mat.png', dpi=300)
+
+c = Counter(i for i in s1)
+print([(emotions[i], c[i]) for i in range(10)])
